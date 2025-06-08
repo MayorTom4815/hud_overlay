69cd1f886c96219c8408830ea961763dc19462cc
Cat-Not-Furry <rodripachecolopez190@gmail.com>
2025-06-07 23:43:27 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import threading
 import sys
 import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs"))
 import config  # Necesario para mutar config.BUTTON_FORMAT
 import json
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
